2d02135a50d4dbba1df9b0a58ee6204096ec8364
Jay Chiarella <jay.gch93@gmail.com>
2021-02-20 15:15:30 +0100

    [main] Add swagger REST API docs
    on branch [main]

--- src/controllers/virtualAgent.ts
+++ src/controllers/virtualAgent.ts
@@ -1,8 +1,12 @@
+import { Get, Route } from "tsoa";
+
 interface VirtualAgentResponse {
     message: string;
 }
 
+@Route("virtualAgent")
 export default class VirtualAgentController {
+    @Get("/greeting")
     public async getGreeting(): Promise<VirtualAgentResponse> {
         return {
             message: "Hey there, I'm the ultimate.ai Virtual agent. Which city are you looking to learn more about today?",
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import VirtualAgentController from "../controllers/virtualAgent";
 
 const router = express.Router();
 
-router.get("/va/greeting", async (_req, res) => {
+router.get("/virtualAgent/greeting", async (_req, res) => {
     const controller = new VirtualAgentController();
     const response = await controller.getGreeting();
     return res.send(response);
